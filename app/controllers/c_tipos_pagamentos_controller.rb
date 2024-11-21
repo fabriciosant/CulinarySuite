5092a8181c171c072b475b58f96e7f9bb9ede12d
Fabricio <fabriciosan47@gmail.com>
2024-11-21 10:44:35 -0400

    Estilizado views de tipos de pagamentos

--- app/controllers/c_tipos_pagamentos_controller.rb
+++ app/controllers/c_tipos_pagamentos_controller.rb
@@ -1,6 +1,5 @@
 class CTiposPagamentosController < ApplicationController
   before_action :set_c_tipo_pagamento, only: %i[ show edit update destroy ]
-
   # GET /c_tipos_pagamentos or /c_tipos_pagamentos.json
   def index
     @c_tipos_pagamentos = CTipoPagamento.all
@@ -23,47 +22,45 @@ class CTiposPagamentosController < ApplicationController
   def create
     @c_tipo_pagamento = CTipoPagamento.new(c_tipo_pagamento_params)
 
-    respond_to do |format|
-      if @c_tipo_pagamento.save
-        format.html { redirect_to c_tipos_pagamentos_path, notice: "C tipo pagamento foi criado com sucesso." }
-        format.json { render :index, status: :created, location: @c_tipo_pagamento }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @c_tipo_pagamento.errors, status: :unprocessable_entity }
-      end
+    if @c_tipo_pagamento.save
+      redirect_to c_tipos_pagamentos_path, notice: "Tipo de pagamento '#{@c_tipo_pagamento.descricao}' criado com sucesso!"
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /c_tipos_pagamentos/1 or /c_tipos_pagamentos/1.json
   def update
-    respond_to do |format|
-      if @c_tipo_pagamento.update(c_tipo_pagamento_params)
-        format.html { redirect_to @c_tipo_pagamento, notice: "C tipo pagamento was successfully updated." }
-        format.json { render :show, status: :ok, location: @c_tipo_pagamento }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @c_tipo_pagamento.errors, status: :unprocessable_entity }
-      end
+    if @c_tipo_pagamento.update(c_tipo_pagamento_params)
+      redirect_to c_tipos_pagamentos_path
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
   # DELETE /c_tipos_pagamentos/1 or /c_tipos_pagamentos/1.json
   def destroy
-    @c_tipo_pagamento.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to c_tipos_pagamentos_path, notice: "tipo de pagamento was successfully destroyed." }
+    if @c_tipo_pagamento.destroy
+      redirect_to c_tipos_pagamentos_path, notice: "Tipo de pagamento '#{@c_tipo_pagamento.descricao}' deletado com sucesso!"
+    else
+      redirect_to c_tipos_pagamentos_path, alert: "Erro ao deletar o tipo de pagamento"
     end
   end
 
   private
+
+    def descricao
+      @c_tipo_pagamento&.descricao || "Descricao não disponivel"
+    end
+
     # Use callbacks to share common setup or constraints between actions.
     def set_c_tipo_pagamento
-      @c_tipo_pagamento = CTipoPagamento.find(params.expect(:id))
+      @c_tipo_pagamento = CTipoPagamento.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
     def c_tipo_pagamento_params
       params.expect(c_tipo_pagamento: [ :descricao ])
+      params.require(:c_tipo_pagamento).permit(:descricao)
     end
 end
