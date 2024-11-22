57890ef773b47d559838537b5850a053df5637ae
Fabricio <fabriciosan47@gmail.com>
2024-11-22 12:48:36 -0400

    atualizado front end pagina estados

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,16 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 import "bootstrap"
+
+
+//#region Confirmar deleção
+window.confirmDelete = function (itemName, deleteUrl) {
+    document.getElementById('modalMessage').textContent = `Você realmente deseja deletar "${itemName}"?`;
+  
+    const form = document.getElementById('deleteForm');
+    form.action = deleteUrl;
+  
+    const modal = new bootstrap.Modal(document.getElementById('deleteModal'));
+    modal.show();
+  };
+//#endregion
