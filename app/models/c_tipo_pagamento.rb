2ebfb59fa642d5e4129b77cbd8b6cb9b263d6094
Kayke <kaykepereira@seduc.ro.gov.br>
2024-12-04 09:49:53 -0400

    validacoes imnput cidades e tipos de pagamento

--- app/models/c_tipo_pagamento.rb
+++ app/models/c_tipo_pagamento.rb
@@ -1,2 +1,19 @@
 class CTipoPagamento < ApplicationRecord
+    validates :descricao, presence: { message: "não pode ficar em branco" }
+    validates :descricao, uniqueness: { case_sensitive: false, message: "já está em uso"}
+    validates :descricao, format: {
+        with: /\A[^0-9]+\z/,
+        message: "não pode conter números"
+    }
+    validate :validar_descricao
+private
+    def validar_descricao
+        if descricao.present? && descricao.length <3
+        errors.add( :descricao, "deve conter pelo menos 3 caracteres")
+        end
+        descricao_normalizada = descricao.unicode_normalize(:nfkd).gsub(/[^\x00-\x7F]/n, '')
+        if descricao != descricao_normalizada
+            errors.add(:descricao, "não pode conter acentuação")
+        end    
+    end
 end
