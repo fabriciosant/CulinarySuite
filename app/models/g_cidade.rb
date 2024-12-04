625e479d4c06fa48a270a3052a287cd940993b0e
Kayke <kaykepereira@seduc.ro.gov.br>
2024-12-04 09:49:53 -0400

    validacoes imnput cidades e tipos de pagamento

--- app/models/g_cidade.rb
+++ app/models/g_cidade.rb
@@ -1,6 +1,23 @@
 class GCidade < ApplicationRecord
   belongs_to :g_estado
 
+  #validações
+  validates :nome, presence: { message: "não pode ficar em branco" }
+  validates :nome, uniqueness: { scope: :g_estado_id, case_sensitive: false, message: "já está em uso no estado" }
+  validates :nome, length: { minimum: 3, maximum: 20, message: "deve ter entre 3 e 20 caracteres" }
+  validates :nome, format: { with: /\A[a-zA-Z\s]+\z/, message: "deve conter apenas letras e espaços " }
+  validate :validar_acentuacao
+  validates :g_estado, presence: {message: "precisa estar associado a um estado válido" }
+
+  private
+
+  def validar_acentuacao
+    nome_normalizado = nome.unicode_normalize(:nfkd).gsub(/[^\x00-\x7F]/n, '')
+    if nome != nome_normalizado
+      errors.add(:nome, "não pode conter caracteres acentuados incorretos")
+    end
+  end
+      
   def to_s
     nome
   end
