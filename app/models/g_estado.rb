ff8bf2a143ffccb6bb3c4cd8ae51d2e8d68fcec7
Kayke <kaykepereira@seduc.ro.gov.br>
2024-12-09 13:17:57 -0400

    validando estado

--- app/models/g_estado.rb
+++ app/models/g_estado.rb
@@ -1,12 +1,18 @@
 class GEstado < ApplicationRecord
   has_many :g_cidades
 
+  # Validações
+  validates :nome, presence: true, uniqueness: true, 
+  format: { with: /\A[\p{L}\p{M}\s]+\z/, message: "deve conter apenas letras e caracteres acentuados" }, 
+  length: { in: 3..100 }
+
+
   def to_s
     nome
   end
 
   def cidades
     estado = GEstado.find(params[:id])
-    render json: estado.cidades.select(:id, :nome)
+    render json: estado.g_cidades.select(:id, :nome)
   end
 end
